421f4d1bfe5c3a2917b05367c20aed3eb14dbcec
bflorestal <yayou7710@hotmail.fr>
2023-05-10 19:17:11 +0200

    Création d'un film

--- server/src/controllers/movie.controller.ts
+++ server/src/controllers/movie.controller.ts
@@ -30,3 +30,15 @@ export async function getMovieById(req: Request, res: Response) {
     return res.status(500).json({ message: "Impossible de récupérer le film" });
   }
 }
+
+export async function createMovie(req: Request, res: Response) {
+  try {
+    const movie = await Movie.create(req.body);
+
+    return res.status(201).json(movie);
+  } catch (err) {
+    if (err instanceof Error) console.error(`${err.name}: ${err.message}`);
+
+    return res.status(500).json({ message: "Impossible de créer le film" });
+  }
+}
--- server/src/routes/movie.route.ts
+++ server/src/routes/movie.route.ts
@@ -1,9 +1,20 @@
 import { Router } from "express";
 
+import {
+  getAllMovies,
+  getMovieById,
+  createMovie,
+  //   updateMovie,
+  //   deleteMovie,
+} from "../controllers/movie.controller";
+
 const router = Router();
 
-router.get("/movies", (_req, res) => {
-  res.send("Hello World!");
-});
+router
+  .get("/movies", getAllMovies)
+  .get("/movies/:id", getMovieById)
+  .post("/movies", createMovie);
+//   .put("/movies/:id", updateMovie)
+//   .delete("/movies/:id", deleteMovie);
 
 export { router as movieRouter };
