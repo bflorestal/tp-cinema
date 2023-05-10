71ff34469818ef65851256e1ce6f40732939f93e
bflorestal <yayou7710@hotmail.fr>
2023-05-10 20:13:11 +0200

    Mise en place routeur

--- client/src/App.tsx
+++
@@ -1,9 +0,0 @@
-export default function App() {
-  return (
-    <main>
-      <section>
-        <h1 className="text-3xl font-bold">Hello world!</h1>
-      </section>
-    </main>
-  );
-}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import Home from "./pages/Home.tsx";
+import Movies from "./pages/Movies.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/movies",
+    element: <Movies />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
