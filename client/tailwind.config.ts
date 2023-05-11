dadb8f4f4d3c7fda8a52dc5ed6cf81ebab8f459b
bflorestal <yayou7710@hotmail.fr>
2023-05-11 09:55:29 +0200

    Ajout classe container

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -4,6 +4,17 @@ import daisyui from "daisyui";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1400px",
+      },
+    },
     extend: {},
   },
   plugins: [daisyui],
