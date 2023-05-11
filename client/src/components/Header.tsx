be3d217c01f31af39fb6ba5a443b03c4f6217c57
bflorestal <yayou7710@hotmail.fr>
2023-05-11 09:31:50 +0200

    Header sticky

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="navbar bg-base-100">
+    <header className="navbar sticky top-0 z-30 bg-opacity-90 backdrop-blur transition-all duration-100 bg-base-100">
       <div className="flex-1">
         <Link to="/" className="btn btn-ghost normal-case text-xl">
           TP Cinéma
