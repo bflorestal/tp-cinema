8349a45173be7140695d253611b45879ccddd5a1
bflorestal <yayou7710@hotmail.fr>
2023-05-11 09:55:35 +0200

    Ajout lorem

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,8 +3,18 @@ import Layout from "../components/Layout";
 export default function Home() {
   return (
     <Layout>
-      <section>
-        <h1 className="text-3xl font-bold">Hello world!</h1>
+      <section className="container flex flex-col items-center justify-center py-8">
+        <div className="space-y-8">
+          <h1 className="text-3xl font-bold text-center">Hello world!</h1>
+          <p className="text-center md:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum ea
+            voluptates recusandae autem aliquid earum distinctio, nisi
+            temporibus iusto. Nihil quia delectus, neque voluptatum facere
+            aperiam velit voluptatem cumque quod suscipit rem modi totam
+            corrupti a eaque perspiciatis praesentium magnam, tenetur est saepe
+            iusto tempore libero! Molestiae minima nemo quod!
+          </p>
+        </div>
       </section>
     </Layout>
   );
